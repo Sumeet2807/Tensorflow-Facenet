87ce57f02c7cf3d03d513077a694d56438c116de
Sumeet <sumeet2807a@gmail.com>
2020-02-11 13:36:59 +0530

    seperated mtcnn initialization and processing

--- util.py
+++ util.py
@@ -238,5 +238,48 @@ def get_distinct_faces(embeddings,max_clusters=20, opt_cluster_threshold=8):
     return(kmeans.cluster_centers_)
 
 
+def load_mtcnn(gpu_memory_fraction=1):
+    print('Creating networks and loading parameters')
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+    return(pnet,rnet,onet)
+
+
+def detect_faces_mtcnn(pnet,rnet,onet,image_paths, image_size=160, margin=44):
+    minsize = 20 # minimum size of face
+    threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
+    factor = 0.709 # scale factor
+    
+
+    tmp_image_paths=copy.copy(image_paths)
+    img_list = []
+    imgs_with_faces = []
+    for image in tmp_image_paths:
+        img = misc.imread(os.path.expanduser(image), mode='RGB')
+        img_size = np.asarray(img.shape)[0:2]
+        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        if len(bounding_boxes) < 1:
+          image_paths.remove(image)
+          print("can't detect face, remove ", image)
+          continue
+        imgs_with_faces.append(image)  
+        det = np.squeeze(bounding_boxes[0,0:4])
+        bb = np.zeros(4, dtype=np.int32)
+        bb[0] = np.maximum(det[0]-margin/2, 0)
+        bb[1] = np.maximum(det[1]-margin/2, 0)
+        bb[2] = np.minimum(det[2]+margin/2, img_size[1])
+        bb[3] = np.minimum(det[3]+margin/2, img_size[0])
+        cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+        aligned = misc.imresize(cropped, (image_size, image_size),  interp='bilinear')
+        prewhitened = facenet.prewhiten(aligned)
+        img_list.append(prewhitened)
+    images = np.stack(img_list)
+    return(images,imgs_with_faces)
+
+
+
 
     
